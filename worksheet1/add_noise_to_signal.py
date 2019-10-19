1196607c7f5a531f33bf33de43004047de8a6226
avesk <averykushner@gmail.com>
2019-10-18 19:16:34 -0700

    removed worksheeg

--- worksheet1/add_noise_to_signal.py
+++
@@ -1,9 +0,0 @@
-import soundfile as sf
-import numpy as np
-
-data, samplerate = sf.read('Market Square.wav')
-noise = np.random.normal(scale=0.5, size=data.shape)
-
-mix = noise + data
-
-sf.write('noisey_song.flac', mix, samplerate)
